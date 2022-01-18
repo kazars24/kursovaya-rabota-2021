69ac6142a00c007a71393bbaa2838e2ef18c849e
kazars24 <71794320+kazars24@users.noreply.github.com>
2022-01-18 16:46:46 +0300

    Распараллелил ffmpeg и ml в два потока.

--- framer.py
+++ framer.py
@@ -3,15 +3,53 @@ import argparse
 import sys
 import enum
 import cv2 as cv
+import time
 
 import ffmpeg
 
 from pathlib import Path
+from threading import Thread
 
 
 class ReturnCode(enum.Enum):
     SUCCESS = 0
     CRITICAL = 1
+
+
+def ffmpeg_for_threading(path_to_video, path_to_outputs):
+    try:
+        # Directories and files
+        output_path = Path(path_to_outputs)
+        filename, _ = os.path.splitext(os.path.basename(path_to_video))
+
+        # Video params
+        probe = ffmpeg.probe(path_to_video)
+        video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+        total_frames = int(video_stream.get('nb_frames', 1000))
+        duration = int(float(video_stream['duration']))
+        width = int(video_stream['width'])
+        height = int(video_stream['height'])
+
+        for sec in range(duration):
+            out, err = (
+                ffmpeg
+                    .input(path_to_video, ss=sec)
+                    # .filter_('select', 'gte(n,{})'.format(frame_num))
+                    .output(f"{path_to_outputs}/{filename}-{sec}.png", vframes=1, format='image2', vcodec='mjpeg')
+                    .run(capture_stdout=True)
+            )
+            print(err)
+            """out, _ = (
+                ffmpeg
+                .input(args.input)
+                .filter('fps')
+                .output(f"{args.output}/{filename}-%d.jpg", start_number=0, vframes=1, format='image2', vcodec='mjpeg')
+                .overwrite_output()
+                .run(quiet=True)
+             )"""
+    except Exception as ex:
+        raise ex
+        exit_code = ReturnCode.CRITICAL
 
 
 def ml_module(path_to_img, path_to_coordinates):
@@ -38,6 +76,21 @@ def ml_module(path_to_img, path_to_coordinates):
         f.write("\n".join(" ".join(map(str, x[1])) for x in objects))
 
 
+def ml_for_treating(output_path):
+    if not os.path.exists(f"{output_path}/coordinates"):
+        os.makedirs(f"{output_path}/coordinates")
+    files = os.listdir(output_path)
+    files.remove('coordinates')
+    while files:
+        for image in files:
+            output = f"{output_path}/coordinates/{image[0:-4]}.txt"
+            ml_module(f"{output_path}/{image}", output)
+            print(image, 'detected!')
+            os.remove(f"{output_path}/{image}")
+        files = os.listdir(output_path)
+        files.remove('coordinates')
+
+
 def main():
     exit_code = ReturnCode.SUCCESS
     script = Path(__file__)
@@ -47,45 +100,16 @@ def main():
     parser.add_argument('-o', '--output', required=True, help='Output dir (.txt if --numpy)')
 
     args = parser.parse_args()
-    try:
-        # Directories and files
-        output_path = Path(args.output)
-        filename, _ = os.path.splitext(os.path.basename(args.input))
+    output_path = Path(args.output)
 
-        # Video params
-        probe = ffmpeg.probe(args.input)
-        video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-        total_frames = int(video_stream.get('nb_frames', 1000))
-        duration = int(float(video_stream['duration']))
-        width = int(video_stream['width'])
-        height = int(video_stream['height'])
+    thread1 = Thread(target=ffmpeg_for_threading, args=(args.input, args.output))
+    thread2 = Thread(target=ml_for_treating, args=(output_path,))
 
-        for sec in range(duration):
-            out, err = (
-                ffmpeg
-                    .input(args.input, ss=sec)
-                    # .filter_('select', 'gte(n,{})'.format(frame_num))
-                    .output(f"{args.output}/{filename}-{sec}.png", vframes=1, format='image2', vcodec='mjpeg')
-                    .run(capture_stdout=True)
-            )
-            print(err)
-        """out, _ = (
-            ffmpeg
-            .input(args.input)
-            .filter('fps')
-            .output(f"{args.output}/{filename}-%d.jpg", start_number=0, vframes=1, format='image2', vcodec='mjpeg')
-            .overwrite_output()
-            .run(quiet=True)
-        )"""
-    except Exception as ex:
-        raise ex
-        exit_code = ReturnCode.CRITICAL
-
-    files = os.listdir(output_path)
-    for image in files:
-        output = f"{output_path}/{image[0:-4]}.txt"
-        ml_module(f"{output_path}/{image}", output)
-        print(image, 'detected!')
+    thread1.start()
+    time.sleep(5)
+    thread2.start()
+    thread1.join()
+    thread2.join()
 
     return exit_code.value
 
