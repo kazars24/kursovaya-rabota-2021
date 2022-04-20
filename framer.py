ad75cbdb871993501598de12d2cf82db6be12c01
kazars24 <kazanzev123@yandex.ru>
2022-04-20 14:19:46 +0300

    Добавил encoder, метрики (размер, битрейт, разрешение, PSNR). Исправил postproc.

--- framer.py
+++ framer.py
@@ -6,6 +6,7 @@ import time
 import numpy as np
 import subprocess as sp
 import ffmpeg
+import os
 
 from pathlib import Path
 from threading import Thread
@@ -20,33 +21,26 @@ class ReturnCode(enum.Enum):
     CRITICAL = 1
 
 
-def ffmpeg_for_threading(path_to_video):
+def ffmpeg_for_threading(path_to_video, path_to_outputs):
     global frames_list, WIDTH, HEIGHT
 
     print('ffmpeg: video processing started')
     ffmpeg_start_time = time.time()
 
     try:
-        # Directories and files
-        # filename, _ = os.path.splitext(os.path.basename(path_to_video))
-
-        # Video params
         probe = ffmpeg.probe(path_to_video)
         video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
         WIDTH = int(video_stream['width'])
         HEIGHT = int(video_stream['height'])
 
-        # ffmpeg command
         command = ['ffmpeg',
                    '-i', path_to_video,
-                   '-pix_fmt', 'bgr24',  # brg24 for matching OpenCV
-                   '-f', 'rawvideo',
-                   'pipe:']
-
-        # Execute FFmpeg as sub-process with stdout as a pipe
-        process = sp.Popen(command, stdout=sp.PIPE, stderr=sp.STDOUT)  # stdout
+                   f'{path_to_outputs}/%03d.jpg'
+                   ]
+        process = sp.Popen(command, stdout=sp.PIPE, stderr=sp.STDOUT)
 
         # Read decoded video frames from the PIPE until no more frames to read
+        '''
         while True:
             # Read decoded video frame (in raw video format) from stdout process.
             buffer = process.stdout.read(WIDTH * HEIGHT * 3)
@@ -57,6 +51,7 @@ def ffmpeg_for_threading(path_to_video):
 
             img = np.frombuffer(buffer, np.uint8).reshape(HEIGHT, WIDTH, 3)
             frames_list.append(img)
+        '''
 
         print("ffmpeg: processing was completed in %s seconds" % (time.time() - ffmpeg_start_time))
 
@@ -64,7 +59,7 @@ def ffmpeg_for_threading(path_to_video):
         raise ex
 
 
-def ml_module(frame, path_to_coordinates):
+def ml_module(path_to_img, path_to_coordinates):
     global WIDTH, HEIGHT
 
     classes_for_task = ['person', 'car', 'bus', 'truck']
@@ -73,6 +68,8 @@ def ml_module(frame, path_to_coordinates):
     net.setInputSize(WIDTH - (WIDTH % 32), HEIGHT - (HEIGHT % 32))
     net.setInputScale(1.0 / 255)
     net.setInputSwapRB(True)
+
+    frame = cv.imread(path_to_img)
 
     with open('ml_cfg/coco.names', 'rt') as f:
         names = f.read().rstrip('\n').split('\n')
@@ -99,18 +96,15 @@ def ml_for_treating(output_path):
     if not coordinates_path.exists():
         coordinates_path.mkdir(parents=True)
 
-    frame_iter = iter(frames_list)
-    index = 0
-    while True:
-        frame = next(frame_iter, False)
-        if isinstance(frame, np.ndarray):
-            output = f"{output_path}/coordinates/frame-{index}.txt"
-            frame_start_time = time.time()
-            ml_module(frame, output)
-            print(f"model: frame-{index} processing ended (%s sec)" % (time.time() - frame_start_time))
-            index += 1
-        else:
-            break
+    files = os.listdir(output_path)
+    files.remove('coordinates')
+    index = 1
+    for image in files:
+        output = f"{output_path}/coordinates/frame-{index}.txt"
+        frame_start_time = time.time()
+        ml_module(f"{output_path}/{image}", output)
+        print(f"model: frame-{index} processing ended (%s sec)" % (time.time() - frame_start_time))
+        index += 1
 
     print("model: processing was completed in %s seconds" % (time.time() - model_start_time))
 
@@ -121,13 +115,15 @@ def postproc(path_to_coordinates, base_qp):
 
     folder = Path(f"{path_to_coordinates}/coordinates")
     folder_len = sum(1 for _ in folder.iterdir())
+
     all_regions = []
-    for i in range(folder_len):
+    for i in range(1, folder_len + 1):
         path_to_file = path_to_coordinates + f"/coordinates/frame-{i}.txt"
         with open(path_to_file) as f:
             objects = f.read().splitlines()
         objects = [list(map(int, x.split(' '))) for x in objects]
-        regions = [f"{x[0]},{x[1] - x[3]},{x[0] + x[2]},{x[1]}:{base_qp}" for x in objects]
+
+        regions = [f"{x[0]},{x[1]},{x[0] + x[2]},{x[1] + x[3]}:{base_qp}" for x in objects]
         regions = ' '.join(regions)
         all_regions.append(regions)
 
@@ -136,7 +132,25 @@ def postproc(path_to_coordinates, base_qp):
     return ' '.join(all_regions)
 
 
+def encoder(path_to_video, regions):
+    print('encoder: started')
+    encoder_start_time = time.time()
+
+    path_to_h264 = f"{path_to_video[:-4]}.h264"
+    command1 = f'ffmpeg -i {path_to_video} -codec:v libx264 -pix_fmt yuv420p -preset slow -qp 10 -an {path_to_h264}'
+    process1 = sp.run(command1, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT)
+
+    path_to_roi = f'{path_to_h264[:-5]}_roi.h264'
+    command2 = f"h264-roi-build/h264_roi {path_to_h264} {path_to_roi} -c 251 -q 50 {regions}"
+    # print(command2)
+    process2 = sp.run(command2, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT)
+
+    print("encoder: processing was completed in %s seconds" % (time.time() - encoder_start_time))
+
+
 def main():
+    print("Starting pipeline")
+    start_time = time.time()
     exit_code = ReturnCode.SUCCESS
     script = Path(__file__)
 
@@ -147,7 +161,7 @@ def main():
     args = parser.parse_args()
     output_path = Path(args.output)
 
-    thread1 = Thread(target=ffmpeg_for_threading, args=(args.input,))
+    thread1 = Thread(target=ffmpeg_for_threading, args=(args.input, output_path))
     thread2 = Thread(target=ml_for_treating, args=(output_path,))
 
     thread1.start()
@@ -156,8 +170,38 @@ def main():
     thread1.join()
     thread2.join()
 
-    regions = postproc(args.output, 51)
-    print(regions)
+    regions = postproc(args.output, 0)
+
+    encoder(args.input, regions)
+
+    print("Pipeline finished in %s seconds" % (time.time() - start_time))
+
+    print('Original video:')
+    og_stat_size = Path(args.input).stat().st_size
+    print(f'- file size: {og_stat_size} bytes')
+    probe = ffmpeg.probe(args.input)
+    video_stream = next(s for s in probe['streams'] if s['codec_type'] == 'video')
+    bitrate = int(int(video_stream['bit_rate']))
+    print(f'- bit rate: {bitrate} bit/s')
+    w = int(video_stream['width'])
+    h = int(video_stream['height'])
+    print(f'- resolution: {w}x{h}')
+
+    print('ROI video:')
+    roi_stat_size = Path(f'{args.input[:-4]}_roi.h264').stat().st_size
+    print(f'- file size: {roi_stat_size} bytes')
+    probe = ffmpeg.probe(f'{args.input[:-4]}_roi.h264')
+    video_stream = next(s for s in probe['streams'] if s['codec_type'] == 'video')
+    # bitrate = int(int(video_stream['bit_rate']))
+    # print(f'- bit rate: {bitrate} bit/s')
+    w = int(video_stream['width'])
+    h = int(video_stream['height'])
+    print(f'- resolution: {w}x{h}')
+    cmd = f"ffmpeg -i {args.input} -i {f'{args.input[:-4]}_roi.h264'} -filter_complex 'psnr' -f null /dev/null"
+    process = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT)
+    psnr = process.stdout.readlines()
+    psnr = psnr[-1].decode('utf-8').split()[7]
+    print('PSNR', psnr)
 
     return exit_code.value
 
